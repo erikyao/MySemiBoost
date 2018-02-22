8fc145e47237b44f8d96299b926a8969b319464e
Yao Yao <yaoy.cs@gmail.com>
2018-02-21 16:09:59 -0800

    deleted unused file

--- msb/sampling.py
+++
@@ -1,5 +0,0 @@
-# def sample_pseudo_weights(pseudo_weights, percent):
-#     sorted_weights = pseudo_weights.sort_values(ascending=False)
-#     top_n = round(len(pseudo_weights) * percent)
-#
-#     return sorted_weights.index[0:top_n]
